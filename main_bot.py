64b927ec958f503b3efe62cf658d27dbaff85229
Nadya Kobylskaya <nadezdakobylskaa2799@gmail.com>
2025-05-25 13:10:35 +0300

    feat: добавлена система тестирования и ведения журнала

--- main_bot.py
+++ main_bot.py
@@ -4,11 +4,17 @@ from aiogram.fsm.storage.memory import MemoryStorage
 
 from app.handlets import router
 
+from logs import logger
+
 async def main():
     bot = Bot(token='7634357678:AAFgDfzMH2ElkGaUGkIBwNXsYkXocNmgEnE')
     dp = Dispatcher()
     dp.include_router(router)
     await dp.start_polling(bot)
+
+
+logger.info("Прогресс успешно сохранён.")
+logger.error(f"Ошибка при получении заданий с ФИПИ: {e}")
 
 if __name__ == '__main__':
     try:
